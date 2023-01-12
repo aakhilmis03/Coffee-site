a5e94959f2b97a5df18c3a6b4cf39fc22e22a0cd
Akhil Mishra <122519273+aakhilmis03@users.noreply.github.com>
2023-01-12 14:24:48 +0530

    Add files via upload

---
+++ COFFEE/scrpit.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchbox.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+let searchbox = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchbox.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchbox.classList.remove('active');
+
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchbox.classList.remove('active');
+    cartItem.classList.remove('active');
+}
